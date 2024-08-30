f0e43709bd0d8c94eebc7de96ab036d37a63b6a6
Hfayed Ben Zekri <hfayed.benzekri@elev.ga.ntig.se>
2024-08-30 13:57:01 +0200

    första

---
+++ Lucktext/Program.cs
@@ -0,0 +1,76 @@
+﻿
+string personname;
+
+string animal1;
+
+string animal2;
+
+string adjective1;
+
+string adjective2;
+
+string adjective3;
+
+string adjective4;
+
+string adjective5;
+
+string adjective6;
+
+string noun1;
+
+string noun2;
+
+string noun3;
+
+string verb;
+
+string pluralnoun;
+
+
+
+Console.WriteLine("Write adjective");
+adjective1 = Console.ReadLine();
+
+Console.WriteLine("Write name");
+personname = Console.ReadLine();
+
+Console.WriteLine("name an adjective");
+adjective2 = Console.ReadLine();
+
+Console.WriteLine("name an Animal");
+animal1 = Console.ReadLine();
+
+Console.WriteLine("write a verb");
+verb = Console.ReadLine();
+
+Console.WriteLine("Write a noun");
+noun1 = Console.ReadLine();
+
+Console.WriteLine("write adjective");
+adjective3 = Console.ReadLine();
+
+Console.WriteLine("write and adjective");
+adjective4 = Console.ReadLine();
+
+Console.WriteLine("name an animal");
+animal2 = Console.ReadLine();
+
+Console.WriteLine("write a noun");
+noun2 = Console.ReadLine();
+
+Console.WriteLine("write a plural noun");
+pluralnoun = Console.ReadLine();
+
+Console.WriteLine("Name an adjective");
+adjective5 = Console.ReadLine();
+
+Console.WriteLine("write a noun");
+noun3 = Console.ReadLine();
+
+Console.WriteLine("Write an adjective");
+adjective6 = Console.ReadLine();
+
+
+Console.WriteLine($"Yesterday, I went on a safari with my {adjective1} friend {personname}. We saw a {adjective2} {animal1} that was {verb} near a {noun1}. Then, a {adjective3} {animal2} appeared out of nowhere, and it startled us! We took some {adjective4} photos of a {noun2} and then fed some {pluralnoun} with the guide's help. Before the day ended, we found a {noun3} hidden under a {adjective5} tree. It was a {adjective6} adventure that I'll never forget!");
+Console.ReadLine();
